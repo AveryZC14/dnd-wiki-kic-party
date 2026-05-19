f6215e73e680b21381f0de0d7328a915365f60f6
Avery Cheng <AveryZC14@gmail.com>
2026-05-20 03:02:15 +1000

    add properties

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,6 +22,7 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
+    Component.Properties(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
@@ -49,7 +50,9 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+   beforeBody: [
+    
+    Component.Breadcrumbs(), Component.ArticleTitle(),  Component.ContentMeta(), ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
